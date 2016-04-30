c5fbaf2366138a6a738b03ccd62e169fce18b44e
Stoyko Petrov <stoyko_petrov@mail.bg>
2016-04-30 23:14:36 +0300

    project info done

--- app/js/controllers/project/InfoProjectController.js
+++ app/js/controllers/project/InfoProjectController.js
@@ -6,21 +6,32 @@ app.controller('InfoProjectController', [
     'projectService',
     'issueService',
     'notifyService',
-    function ($scope, $routeParams, projectService, issueService, notifyService) {
+    'PAGE_SIZE',
+    function ($scope, $routeParams, projectService, issueService, notifyService, PAGE_SIZE) {
 
         projectService.getProjectById($routeParams.id)
             .then(function (projectData) {
+                projectData.PrioritiesToString = projectData.Priorities.map(function(priority) {return priority.Name;}).join(', ');
+                projectData.LabelsToString = projectData.Labels.map(function(label) {return label.Name;}).join(', ');
                 $scope.project = projectData;
-                issueService.getIssuesByProjectId(projectData.Id)
-                    .then(function (issuesData) {
-                        console.log(issuesData);
-                        $scope.issues = issuesData;
 
+                var currentProjectId = projectData.Id;
 
+                $scope.issuesParams = {
+                    pageNumber: 1,
+                    pageSize: PAGE_SIZE
+                };
 
-                    }, function (error) {
-                        notifyService.showError('Load issues failed!', error);
-                    });
+                $scope.reloadIssuesProjectToView = function () {
+                    issueService.getIssuesByProjectId(currentProjectId, $scope.issuesParams)
+                        .then(function (data) {
+                            $scope.issues = data;
+                        }, function (error) {
+                            notifyService.showError('Cannot load project issues', error);
+                        });
+                };
+
+                $scope.reloadIssuesProjectToView();
 
             }, function (error) {
                 notifyService.showError('Load project failed!', error);
--- app/js/services/issueService.js
+++ app/js/services/issueService.js
@@ -6,36 +6,7 @@ app.factory('issueService', [
     'authService',
     'BASE_URL',
     function ($http, $q, authService, BASE_URL) {
-        //function getMyIssues(params) {
-        //    var deferred = $q.defer();
-        //    var filterUrl = 'orderBy=DueDate desc, IssueKey' + '&pageSize=' + params.pageSize + '&pageNumber=' + params.pageNumber;
-        //
-        //    $http.get(BASE_URL + 'Issues/me?' + filterUrl, {headers: authService.getAuthHeaders()})
-        //        .then(function (response) {
-        //            deferred.resolve(response.data);
-        //        }, function (error) {
-        //            deferred.reject(error.data);
-        //        });
-        //
-        //    return deferred.promise;
-        //}
-        //
-        //function getIssueById(id) {
-        //    var deferred = $q.defer();
-        //
-        //    $http.get(BASE_URL + 'Issues/' + id, {headers: authService.getAuthHeaders()})
-        //        .then(function (response) {
-        //            deferred.resolve(response.data);
-        //        }, function (error) {
-        //            deferred.reject(error.data);
-        //        });
-        //
-        //    return deferred.promise;
-        //}
-
-        return {
-            //getMyIssues: getMyIssues,
-            //getIssueById: getIssueById
+         return {
             getMyIssues: function (params) {
                 var deferred = $q.defer();
                 var filterUrl = 'orderBy=DueDate desc, IssueKey' + '&pageSize=' + params.pageSize + '&pageNumber=' + params.pageNumber;
@@ -49,22 +20,23 @@ app.factory('issueService', [
 
                 return deferred.promise;
             },
-            getIssueById: function (id) {
+            //getIssueById: function (id) {
+            //    var deferred = $q.defer();
+            //
+            //    $http.get(BASE_URL + 'Issues/' + id, {headers: authService.getAuthHeaders()})
+            //        .then(function (response) {
+            //            deferred.resolve(response.data);
+            //        }, function (error) {
+            //            deferred.reject(error.data);
+            //        });
+            //
+            //    return deferred.promise;
+            //},
+            getIssuesByProjectId: function (projectId, params ) {
                 var deferred = $q.defer();
+                var filterUrl = 'filter=ProjectId=='+ projectId + '&pageSize=' + params.pageSize + '&pageNumber=' + params.pageNumber;
 
-                $http.get(BASE_URL + 'Issues/' + id, {headers: authService.getAuthHeaders()})
-                    .then(function (response) {
-                        deferred.resolve(response.data);
-                    }, function (error) {
-                        deferred.reject(error.data);
-                    });
-
-                return deferred.promise;
-            },
-            getIssuesByProjectId: function (id) {
-                var deferred = $q.defer();
-
-                $http.get(BASE_URL + 'Projects/' + id + '/Issues', {headers: authService.getAuthHeaders()})
+                $http.get(BASE_URL + 'Issues/?' + filterUrl, {headers: authService.getAuthHeaders()})
                     .then(function (response) {
                         deferred.resolve(response.data);
                     }, function (error) {
